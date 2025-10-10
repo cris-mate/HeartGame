e935a27cdb1bd496f5eaae0984115f8a7c5d1a51
cristian <cristidragosmatei@gmail.com>
2025-10-10 22:02:34 +0100

    create event package containing new GameEventListener interface, GameEventType enum and GameEventManager class

---
+++ IdeaProjects/HeartGame/src/event/GameEventType.java
@@ -0,0 +1,11 @@
+package event;
+
+/**
+ * Defines the types of events that can be published within the game
+ */
+public enum GameEventType {
+    CORRECT_ANSWER_SUBMITTED,
+    INCORRECT_ANSWER_SUBMITTED,
+    GAME_STARTED,
+    PLAYER_LOGGED_IN
+}
