6dd42efd8f4706b44610ef708330e8e78fe15d9c
cristian <cristidragosmatei@gmail.com>
2025-10-12 20:43:11 +0100

    refactor module-info.java to match HeartGame

--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,15 @@
 /**
- * 
+ * HeartGame module definition
  */
-/**
- * 
- */
-module TomatoGame {
+odule HeartGame {
 	requires java.desktop;
     requires java.logging;
+    requires java.sql;
+    requires jbcrypt;
+    requires ch.qos.logback.classic;
+    requires ch.qos.logback.core;
+
+    exports com.heartgame.view;
+    exports com.heartgame.controller;
+    exports com.heartgame.model;
 }
